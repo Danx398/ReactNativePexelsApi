b5b1d90e9452d5cc4ed740627637d37932ece8a9
Danx398 <danidannniel398@gmail.com>
2023-03-30 10:50:24 -0600

    cambios

--- App.js
+++ App.js
@@ -8,45 +8,71 @@ import { Text, Image, StyleSheet } from 'react-native';
 import pexelsLogo from './assets/pexels.jpg'
 
 
-const stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator()
 
 export default function App() {
 
-  const [openSearch, setOpenSearch] = useState(false)
+	const [openSearch, setOpenSearch] = useState(false)
 
 
-  return (
-    <NavigationContainer>
-      <stack.Navigator>
-        <stack.Screen name='HomeScreen' component={HomeScreen}
-          options={{
-            headerLeft: () => <Image source={pexelsLogo} style={styles.logo} />,
-            title: 'Pexels App',
-            headerTitleStyle:{
-              color: '#FFF',
-              fontWeight: 'bold'
-            },
-            headerStyle:{
-              backgroundColor: '#0d0d0d'
-            },
-            headerRight: ()=>(
-              <Text style={{color:'white',fontSize:18}}
-                onPress={()=>setOpenSearch(!openSearch)}
-              >
-                {openSearch ? "Close":"Search"}
-              </Text>
-            )
-          }}
-        />
-        <stack.Screen name='ImageScreen' component={ImageScreen} />
-      </stack.Navigator>
-      <StatusBar />
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen
+					name="Home"
+					options={{
+						headerLeft: () => <Image source={pexelsLogo} style={styles.logo} />,
+						headerRight: () => (
+							<Text
+								style={{ color: "white", fontSize: 18 }}
+								onPress={() => setOpenSearch(!openSearch)}
+							>
+								{openSearch ? "Close" : "Search"}
+							</Text>
+						),
+						title: "Pexels App",
+						headerTintColor: "#fff",
+						headerTitleStyle: {
+							fontWeight: "bold",
+						},
+						headerStyle: {
+							backgroundColor: "#0D0D0D",
+						},
+					}}
+				>
+					{(props) => <HomeScreen {...props} openSearch={openSearch} />}
+				</Stack.Screen>
+				{/* <Stack.Screen name='HomeScreen' component={HomeScreen}
+		  options={{
+			headerLeft: () => <Image source={pexelsLogo} style={styles.logo} />,
+			title: 'Pexels App',
+			headerTitleStyle: {
+			  color: '#FFF',
+			  fontWeight: 'bold'
+			},
+			headerStyle: {
+			  backgroundColor: '#0d0d0d'
+			},
+			headerRight: () => (
+			  <Text style={{ color: 'white', fontSize: 18 }}
+				onPress={() => setOpenSearch(!openSearch)}
+			  >
+				{openSearch ? "Close" : "Search"}
+			  </Text>
+			),
+		  }}
+		> 
+		   {(props) => <HomeScreen {...props} openSearch={openSearch} />}
+		</Stack.Screen> */}
+				<Stack.Screen name='ImageScreen' component={ImageScreen} />
+			</Stack.Navigator>
+			<StatusBar />
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  logo: {
-    width: 37, height: 37, marginEnd: 5, borderRadius: 5
-  }
+	logo: {
+		width: 37, height: 37, marginEnd: 5, borderRadius: 5
+	}
 })
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,41 +2,62 @@ import { View, StyleSheet, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { getImages } from '../api/pexels'
 import ImageList from '../components/ImageList'
+import { Input, Button } from 'react-native-elements'
 
-const HomeScreen = () => {
+const HomeScreen = ({ openSearch }) => {
 
-  const [photos, setPhotos] = useState([])
+	const [photos, setPhotos] = useState([])
 
 
-  const loadImages = async () => {
-    const res = await getImages();
-    // console.log(res.headers);
-    setPhotos(res.data.photos)
-  }
+	const loadImages = async () => {
+		const res = await getImages();
+		// console.log(res.headers);
+		setPhotos(res.data.photos)
+	}
 
-  useEffect(() => {
-    loadImages();
-  }, [])
-  return (
-    <View style={styles.container}>
-      <Text style={styles.totalResultsText}>1000 Results</Text>
-      <ImageList photos={photos} />
-    </View>
-  )
+	useEffect(() => {
+		loadImages();
+	}, [])
+	return (
+		<>
+			{openSearch && (<View style={styles.searchSection}>
+				<Input placeholder='Search Term'
+					style={styles.searchInput}
+				/>
+				<Button title="Search" />
+			</View>)}
+			<View style={styles.container}>
+				<Text style={styles.totalResultsText}>1000 Results</Text>
+				<ImageList photos={photos} />
+			</View>
+		</>
+	)
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0d0d0d',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  totalResultsText: {
-    color: '#D0D0D0',
-    textAlign: 'center',
-    textAlign: 'right',
-    width: '100%',
-    paddingTop:35
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#0d0d0d',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	totalResultsText: {
+		color: '#D0D0D0',
+		textAlign: 'center',
+		textAlign: 'right',
+		width: '100%',
+		paddingTop: 35,
+	},
+	searchInput: {
+		backgroundColor: '#2c292c',
+		borderBottomWidth: 0,
+		paddingHorizontal: 4,
+
+
+	},
+	searchSection:{
+		backgroundColor: '#0d0d0d',
+		width:'100%',
+		paddingLeft:10
+	}
 })
 export default HomeScreen
